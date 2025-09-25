1a276db10f2759ee48f2b5618a615be0add2bf42
Amit Dubey <amitdubey1305@gmail.com>
2025-09-25 22:18:26 +0530

    deploy: Final production configuration

--- web/src/api.js
+++ web/src/api.js
@@ -1,8 +1,6 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  // This is the base URL of your backend server
-  baseURL: 'http://localhost:3001/api',
+  baseURL: '/api',
 });
-
 export default apiClient;
